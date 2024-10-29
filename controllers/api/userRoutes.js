e66691c0c3bb58a8ac19d045ca4697eb4f80de50
melyanalie <melyana.lie86@gmail.com>
2024-10-28 22:01:29 -0400

    added api, server.js and handlebars

---
+++ controllers/api/userRoutes.js
@@ -0,0 +1,44 @@
+const router = require('express').Router();
+const { User } = require('../../models');
+
+router.post('/signup', async (req, res) => {
+  try {
+    const newUser = await User.create(req.body);
+    req.session.save(() => {
+      req.session.userId = newUser.id;
+      req.session.loggedIn = true;
+      res.status(200).json(newUser);
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.post('/login', async (req, res) => {
+  try {
+    const user = await User.findOne({ where: { username: req.body.username } });
+    if (!user || !user.checkPassword(req.body.password)) {
+      res.status(400).json({ message: 'Invalid credentials' });
+      return;
+    }
+    req.session.save(() => {
+      req.session.userId = user.id;
+      req.session.loggedIn = true;
+      res.json({ user, message: 'You are now logged in!' });
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.post('/logout', (req, res) => {
+  if (req.session.loggedIn) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
+});
+
+module.exports = router;
