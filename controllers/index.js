d4ab1b9c64103ac4641a722fb86a19eae9e57e82
melyanalie <melyana.lie86@gmail.com>
2024-11-02 12:32:23 -0400

    fixed controllers folder

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
-const userRoutes = require('./userRoutes');
-const postRoutes = require('./postRoutes');
+const userRoutes = require('./api/userRoutes');
+const postRoutes = require('./api/postRoutes');
 
 router.use('/users', userRoutes);
 router.use('/posts', postRoutes);
