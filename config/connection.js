2bc9b1045ff2cf9ff06a4bdb4bdba1720ab47673
melyanalie <melyana.lie86@gmail.com>
2024-11-02 16:31:39 -0400

    fixed connectionjs

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,13 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: 'mysql',
+const sequelize = new Sequelize('database', 'username', 'password', {
+  host: 'localhost',
+  dialect: 'mysql',
+  port: 3307,
+  dialectOptions: {
+    connectTimeout: 60000 // 60 seconds
   }
-);
+});
 
 module.exports = sequelize;
